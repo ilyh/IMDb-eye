c8bf60fd046bc5be772d2584aa5440f738f6245a
ilyh <chenbo515@gmail.com>
2015-12-11 00:05:39 +0800

    using the new api

--- src/imdb.py
+++ src/imdb.py
@@ -3,29 +3,32 @@ import sys
 import io
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='gb18030')
 
-API_URL = "http://www.myapifilms.com/imdb"
+API_URL = "http://api.myapifilms.com/imdb/idIMDB"
+token = "5a01f13a-f9e4-4df6-bab8-da15354d16eb"
 
 fetchApiByParams = lambda x: requests.get(API_URL, params=x).json()
 
 
 def getMoviesByidName(idName):
     params = {
+        "token": token,
         "idName": idName,
-        "format": "JSON",
+        "format": "json",
         "lang": "en-us",
         "filmography": "1"
     }
-    return fetchApiByParams(params)["filmographies"]
+    return fetchApiByParams(params)["data"]["names"][0]["filmographies"]
 
 
 def getMoviesByName(name):
     params = {
+        "token": token,
         "name": name,
-        "format": "JSON",
+        "format": "json",
         "lang": "en-us",
         "filmography": "1"
     }
-    return fetchApiByParams(params)[0]["filmographies"]
+    return fetchApiByParams(params)["data"]["names"][0]["filmographies"]
 
 
 def getArtistsByMovieid(movieid):
