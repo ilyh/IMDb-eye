07476003c527946db51c53875ef3c57ad6c37279
ilyh <chenbo515@gmail.com>
2015-12-15 16:03:14 +0800

    修改展示效果
    1.增加缩放功能
    2.text显示名字,不显示id
    3.电影节点大小设为15,演员设为8

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,25 @@
 var nodeset = new Set()
 var nodes = []
-var clinks = []
-var links = []
+var nodeInfo = {} //已id为索引的信息
+var clinks = [] //source和target为id的link
+var links = [] //source和target为index的link
 
 var width = document.documentElement.clientWidth,
     height = document.documentElement.clientHeight;
 var x = d3.scale.linear().domain([0, width]).range([0, width]);
-var y = d3.scale.linear().domain([0, height]).range([height, 0]);
-var svg = d3.select("body").append("svg").attr("width", width).attr("height", height).append("g");
+var y = d3.scale.linear().domain([0, height]).range([0, height]);
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 10]).on("zoom", zoom))
+    .on("dblclick.zoom", null);
 var link = svg.selectAll(".link"),
     node = svg.selectAll(".node");
 svg.append("rect").attr("class", "overlay").attr("width", width).attr("height", height);
 var node_drag = d3.behavior.drag().on("dragstart", dragstart).on("drag", dragmove).on("dragend", dragend);
+var force = d3.layout.force();
 
 function dragstart(b, a) {
     force.stop();
@@ -32,10 +40,40 @@ function dragend(b, a) {
     force.resume()
 }
 
+function tick() {
+    node.attr("transform", function(a) {
+        return "translate(" + x(a.x) + "," + y(a.y) + ")"
+    });
+    link.attr("x1", function(a) {
+        return x(a.source.x)
+    }).attr("y1", function(a) {
+        return y(a.source.y)
+    }).attr("x2", function(a) {
+        return x(a.target.x)
+    }).attr("y2", function(a) {
+        return y(a.target.y)
+    })
+}
+
+function zoom() {
+    force.resume();
+    node.attr("transform", function(a) {
+        return "translate(" + x(a.x) + "," + y(a.y) + ")"
+    });
+    link.attr("x1", function(a) {
+        return x(a.source.x)
+    }).attr("y1", function(a) {
+        return y(a.source.y)
+    }).attr("x2", function(a) {
+        return x(a.target.x)
+    }).attr("y2", function(a) {
+        return y(a.target.y)
+    })
+}
 
 $(document).ready(function() {
     $.ajax({
-            url: './nm1670029.json',
+            url: './nm2244205.json',
             type: 'GET',
             dataType: 'json',
         })
@@ -44,15 +82,24 @@ $(document).ready(function() {
             for (k in data) {
                 if (data.hasOwnProperty(k)) {
                     nodeset.add(k); //电影id
+                    nodeInfo[k] = {
+                        type: 'movie',
+                        text: data[k].title
+                    }
                     data[k].artists.forEach(function(x) {
                         nodeset.add(x.actorId); //演员id
                         clinks.push({
                             source: k,
                             target: x.actorId
                         });
+                        nodeInfo[x.actorId] = {
+                            type: 'actor',
+                            text: x.actorName
+                        }
                     })
                 }
             }
+
 
             nodeset.forEach(function(x) {
                 nodes.push({
@@ -85,22 +132,29 @@ function indexOfName(name) {
 
 
 function restart() {
-    var force = d3.layout.force()
-        .nodes(nodes) //指定节点数组
+    force.nodes(nodes) //指定节点数组
         .links(links) //指定连线数组
         .size([width, height]) //指定作用域范围
-        .linkDistance(150) //指定连线长度
-        .charge([-400]); //相互之间的作用力
+        .linkDistance(100) //指定连线长度
+        .charge([-400]) //相互之间的作用力
+        .linkStrength(1);
     force.start();
 
     var color = d3.scale.category20();
     svg.selectAll(".link").remove();
     svg.selectAll(".node").remove();
     link = svg.selectAll(".link").data(links).enter().append("line").attr("class", "link");
-    node = svg.selectAll(".node").data(nodes).enter().append("circle").attr("r",10).style("fill",function(d,i){
-         return color(i);
-     })
-     .call(force.drag);  //使得节点能够拖动
+    node = svg.selectAll(".node").data(nodes).enter().append("circle").attr("r", function(d) {
+            if(nodeInfo[d.name].type==='movie'){
+                return 15;
+            }
+            else{
+                return 8;
+            }
+        }).style("fill", function(d, i) {
+            return color(i);
+        })
+        .call(force.drag); //使得节点能够拖动
     text = svg.selectAll("text")
         .data(nodes)
         .enter()
@@ -109,7 +163,7 @@ function restart() {
         .attr("dx", 20)
         .attr("dy", 8)
         .text(function(d) {
-            return d.name;
+            return nodeInfo[d.name].text;
         });
     force.on("tick", function() {
         node.attr("transform", function(a) {
